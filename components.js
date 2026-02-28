189085980ce1641a2d5285ecb9100c3cb408af1f
Stefan <stefan@ponnykatt.se>
2026-03-01 00:33:18 +0100

    added a farming section

---
+++ components.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('#nav')
+    nav.innerHTML = `
+        <nav>
+            <a href="/index.html">Relic Checker</a>
+            <a href="/farm.html">Farm Tracker</a>
+        </nav>
+    `
+})
